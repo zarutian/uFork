6165a018f5ca4257c31721de0232c35db5a0c61a
Zarutian <zarutian@gmail.com>
2024-06-08 16:12:34 +0000

    Create src2src.js

---
+++ fpga/ucode/util/src2src.js
@@ -0,0 +1,14 @@
+// @ts-check js
+/**
+ * @use JSDoc
+ * @overview This translates from masm structured/formatted code into forth ish source code
+ * @author Zarutian
+ **/
+
+export const makeSrc2srcTranslator = (opts) => {
+
+};
+
+export default {
+  makeSrc2srcTranslator,
+};
